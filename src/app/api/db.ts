c07bc2bec446d874cd7abc96f6502ecf4e79ccac
yusenlin <1558365539@qq.com>
2024-03-25 17:34:00 +0800

    feat: init

---
+++ src/app/api/db.ts
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'
+// 创建数据库连接池以提高性能
+const pool = mysql.createPool({
+	host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
+})
+export default pool
---
+++ src/app/api/route.ts
@@ -0,0 +1,2 @@
+// nextjs连接mysql
+
